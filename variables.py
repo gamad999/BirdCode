d3403a029db639c46d4b707e6b18c17cecc53af1
gamad999 <gamd999@outlook.com>
2023-04-18 18:22:48 -0500

    Lesson 2: Declaration of variables in python

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Creation of variables in Python
+
+name = "Multicoloured tanager"
+nombre = "Tangara multicolor"
+scientific_name = "Chlorochrysa nitidissima"
+weight = 8.5
+wings = 2
+migration = False
+
+print("The scientific name of this bird is: ", scientific_name)
+print()
+print("The english name of this bird is: ", name)
+print()
+print("The spanish name of this bird is: ", nombre)
+print()
+print("The weight of a nest of this species would be: ", (weight*2) + (weight/2)*4, "gr")
+print()
+print("The Multicoloured tanager is an endemic species of Colombia. Migration: ", migration)

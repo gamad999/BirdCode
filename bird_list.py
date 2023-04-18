944787fec5989cc34e9ecd37e21cbb876e884924
gamad999 <gamd999@outlook.com>
2023-04-18 18:35:36 -0500

    Creation of lists in Python

---
+++ bird_list.py
@@ -0,0 +1,14 @@
+# Creation of lists in Python
+
+birds = ["tanagers", "Multicoloured tanager", "Tangara multicolor", 12, 8.5, True]
+
+print("The elements of the birds list are: ")
+print()
+print(birds)
+print()
+print("The english name of this bird is ", birds[1])
+print()
+print("The index of the second object of the birds list is: ", birds.index("Multicoloured tanager"))
+birds.append("Chlorochrysa nitidissima")
+print()
+print(birds)

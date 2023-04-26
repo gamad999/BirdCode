bdea4530aa12ae10b3e09e2a0d624a532d0d40cd
gamad999 <gamard199@gmail.com>
2023-04-25 20:44:11 -0500

    Comments settings

--- welcome.py
+++ welcome.py
@@ -1,5 +1,7 @@
 # Training program in Python to guess bird names
 
+# Ask the user to put down his name
+
 username = input("Enter your name: ")
 
 print("Hello ", username, "welcome to BirdCode!!")

c8560380e8a1c47a15a730093c29faec14f0d93d
gamad999 <gamard199@gmail.com>
2023-04-25 20:49:48 -0500

    Lesson 1: Theme's explanation

--- welcome.py
+++ welcome.py
@@ -1,5 +1,7 @@
 # Training program in Python to guess bird names
 
+# Learn how to allow the user to enter a message and save it in a variable
+
 # Ask the user to put down his name
 
 username = input("Enter your name: ")
